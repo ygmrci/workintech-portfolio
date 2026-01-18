cfb556a764f1a0ae2e4891e8e36f3ef7ee695773
yağmur gamze çimen <yagmurgamzecimen@gmail.com>
2026-01-18 16:52:26 +0300

    deploy: vercel setup

--- src/api/reqres.js
+++ src/api/reqres.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export async function postWorkintech(payload) {
